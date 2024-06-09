567f1e4f8b9325ae3033e1dc2ed64aa825bef915
unknown <aloneviahome@gmail.com>
2024-06-09 16:02:17 +0200

    init

---
+++ js/func.js
@@ -0,0 +1,12 @@
+const sayHello = () => { console.log("Hello World") }
+const add = (a, b) => {
+ console.log(a + b)
+}
+
+const someTitle = () => { return 'Mój tytuł' }
+
+module.exports = {
+ sayHello,
+ add,
+ someTitle
+}
